d1d084f98c1d288ad7457f62267d8472cb95bd72
Bram Kok <bram@bramkok.com>
2016-04-09 15:14:41 +0200

    Add cli interface for msgbox
    Make sure both message and title are supplied

---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const meow = require('meow');
+const msgbox = require('msgbox');
+
+const cli = meow(`
+	Usage
+	  $ msgbox <title> <message>
+
+	Examples
+	  $ msgbox "Try a Clickbait Title" "You wont believe what will happen next!"`);
+
+const title = cli.input[0];
+const message = cli.input[1];
+
+if (typeof title === 'undefined') {
+  console.error('Error: title is missing');
+  console.error('Usage: msgbox <title> <message>');
+  process.exit(1);
+}
+
+if (typeof message === 'undefined') {
+  console.error('Error: message is missing');
+  console.error('Usage: msgbox <title> <message>');
+  process.exit(1);
+}
+
+msgbox(title, message);
+process.exit(1);
---
+++ readme.md
@@ -0,0 +1,17 @@
+# msgbox-cli
+
+> Improved console message output
+
+<img src="screenshot.png" width="569">
+
+## Install
+
+```
+$ npm install -g msgbox-cli
+```
+
+## Usage
+
+```bash
+$ msgbox "The title" "And here goes your message."
+```
